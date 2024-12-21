eceb6ec4a70ff38011201af01c0385f9f5538d71
Andrey <andreysouzastart@gmail.com>
2024-12-21 15:22:37 -0300

    Alteração na arquitetura dos arquivos

--- templates/static/javascript/main.js
+++ templates/static/javascript/main.js
@@ -1,15 +1,13 @@
 // Função para remover mensagens após 5 segundos
 setTimeout(() => {
-    const messages = document.querySelectorAll('.message'); // Seleciona todas as mensagens
+    const messages = document.querySelectorAll('.message');
     messages.forEach((message) => {
-        // Adiciona uma transição suave
         message.style.transition = "opacity 1s ease";
         message.style.opacity = "0"; // Define a opacidade como 0 para desaparecer
 
-        // Remove o elemento do DOM após a transição
         setTimeout(() => {
             message.remove();
         }, 1000); // Aguarda 1 segundo para a transição concluir
     });
-}, 5000); // Aguarda 5 segundos antes de começar a desaparecer
+}, 5000);
 
